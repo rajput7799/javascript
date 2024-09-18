502fbab3776efe62baa5939b60357eab25b1c913
rajput7799 <royalrajputchetan2003@gmail.com>
2024-09-18 19:41:36 +0530

    operators and operations

---
+++ video_7/script.js
@@ -0,0 +1,15 @@
+// let a= 7
+// let b= 4
+// let neg_a= -a
+// console.log(neg_a);
+// let str1 = "Hello,"
+// let str2 = " World!"
+// console.log(str1 + str2);
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+// console.log(+true);
+// console.log(+"");
+let gc = 100, ac = 2;
+ac = +ac + gc;
+gc++;
+console.table([gc, ac]);
