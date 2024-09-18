1a5399f985c0af364dc7c37a6e54aa2cd3a89f4e
rajput7799 <royalrajputchetan2003@gmail.com>
2024-09-18 17:10:49 +0530

    4th vid

---
+++ video_4/script.js
@@ -0,0 +1,11 @@
+const account_id = 14889
+let acc_email = "hello@email.com"
+var password = "hello14889"
+city = "Kotabagh"
+
+// account_id = 13998 // not allowed
+acc_email = "world@email.com"
+password = "world13998"
+city = "Haldwani"
+
+console.table([account_id, acc_email, password, city]);
