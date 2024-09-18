877270815270ccda9436e52644f8f6def4471f51
rajput7799 <royalrajputchetan2003@gmail.com>
2024-09-18 19:13:23 +0530

    Conversion

---
+++ video_6/script.js
@@ -0,0 +1,15 @@
+let age = "21cr";
+
+console.log(typeof age);
+
+let number_value = Number(age);
+console.log(typeof number_value);
+console.log(number_value);
+
+/*
+"33"=>33
+"33abc"=>NaN
+undefined=>NaN
+null=>0
+true/false=>1/0
+*/
