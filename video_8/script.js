db2859e9d2d16590f2f5640af6d265415950e1d2
rajput7799 <royalrajputchetan2003@gmail.com>
2024-09-18 19:58:20 +0530

    comparison

---
+++ video_8/script.js
@@ -0,0 +1,11 @@
+// simple comparisons (<, >, <=, >=, ==, !=)
+console.log("2" > 1);
+console.log("2a" > 1); // ?
+console.log(null >= 0);
+console.log(null == 0);
+console.log(undefined >= 0);
+console.log(undefined == 0);
+
+// ===
+
+console.log("2" === 2);
