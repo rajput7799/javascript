eeff577db20373c8e63b4c333222ea4755993f67
rajput7799 <royalrajputchetan2003@gmail.com>
2024-09-18 17:46:04 +0530

    Datatypes

---
+++ video_5/script.js
@@ -0,0 +1,14 @@
+"use strict"; //treat all JS code as newer version
+
+// number => 2^53 range
+//bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => decalration but no value
+// symbol => unique
+
+// object
+
+console.log(typeof undefined);
+console.log(typeof null);
