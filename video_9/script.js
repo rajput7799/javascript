fb2c869844e8e0af3a3912b280e54e31fb20e135
rajput7799 <royalrajputchetan2003@gmail.com>
2024-09-20 21:03:36 +0530

    datatype summary

---
+++ video_9/script.js
@@ -0,0 +1,28 @@
+/* Primitive => 7 types
+    > Number
+    > Boolean
+    > String
+    > null
+    > undefined
+    > Symbol
+    > BigInt
+
+   Reference(Non-Primitive) => 3 types
+    > Array
+    > Object
+    > Functions
+*/
+// let n = 0;
+// let m = null;
+// let bignum = 98n
+// console.log(typeof bignum);
+
+// let myname = {
+//     name: "chetan",
+//     age: 21,
+// }
+// console.log(myname);
+
+// const newfunction = function name() {
+
+// }
