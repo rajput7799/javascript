e82259cb2479dd496b833ea4c22396ba088bbe62
rajput7799 <royalrajputchetan2003@gmail.com>
2024-09-21 21:44:53 +0530

    Number and Math

---
+++ video_12/script.js
@@ -0,0 +1,23 @@
+// let ac = 199
+// console.log(ac);
+// let ab = new Number(200.6788)
+// console.log(ab);
+// console.log(ab.toFixed(3));
+// console.log(ab.toPrecision(2));
+// let billion = 1000000000
+// console.log(billion.toLocaleString("ja-JP-u-ca-japanese"));
+// console.log(billion.toLocaleString("ar-EG"));
+// const date = new Date(Date.UTC(2012, 11, 20, 3, 0, 0));
+// console.log(date.toLocaleString("ja-JP-u-ca-japanese"));
+
+
+// ++++++++++++++MATHS++++++++++++++
+
+// let pi = Math.PI
+// console.log(pi);
+// let theta = Math.sin(180)
+// console.log(theta);
+// console.log(typeof Math);
+let min = 1
+let max = 6
+console.log((Math.floor(Math.random() * (max - min + 1))) + 1);
