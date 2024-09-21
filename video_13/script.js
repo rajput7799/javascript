b71a0161901fb872e450bc26179049e4e9a9da79
rajput7799 <royalrajputchetan2003@gmail.com>
2024-09-21 22:17:35 +0530

    date in js

---
+++ video_13/script.js
@@ -0,0 +1,9 @@
+let instant_date = new Date();
+console.log(instant_date);
+console.log(instant_date.toString());
+console.log(instant_date.toDateString());
+console.log(instant_date.toJSON());
+console.log(instant_date.toISOString());
+console.log(instant_date.toTimeString());
+console.log(instant_date.toUTCString());
+console.log(instant_date.toLocaleString('en-IN'));
